bda62fc0b0c2adb8b94b8ed5e007b1a6cd44c6ad
Sam Culp <samculp04@yahoo.com>
2026-09-18 17:01:53 -0500

    feat: inital implementation for PCB and process list

---
+++ SimOS/Process.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimOS;
+
+public class Process
+{
+    public enum ProcessState
+    {
+        Initial,
+        Ready,
+        Running,
+        Blocked,
+        Final
+    }
+
+    public int PID { get; }
+    public ProcessState State { get; set; }
+    public int ProgramCounter { get; set; }
+
+    public Process? Parent { get; }
+    public List<Process> Children { get; }
+
+    public Process(int pid, Process? parent = null)
+    {
+        PID = pid;
+        State = ProcessState.Initial;
+        ProgramCounter = 0;
+        Parent = parent;
+        Children = new();
+    }
+}
